a8a5f5781e028ca66ca911bfaf5edb83934f509d
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2025-12-01 18:02:04 +0100

    calcolo prezzo finale

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ console.log(etaPassegero);
 let kmPercorsi = parseInt(prompt("quanti km vuoi percorrere?"))
 console.log ( kmPercorsi);
 
-//se possiedi meno di 18 anni ci sara uno sconto del 20%//
+const costoPerkm= 0.21;
 
-if (etaPassegero<18){
-    kmPercorsi
-} 
+//se possiedi meno di 18 anni ci sara uno sconto del 20%//
+let prezzoFinale = costoPerkm * kmPercorsi;
+console.log(" prezzo finale non scontato" + prezzoFinale )
+
+
+if (etaPassegero < 18){
+    prezzoFinale = prezzoFinale * 0.8
+    
+console.log( "prezzoUnder 18 " +  prezzoFinale )
+}else if (etaPassegero> 65){
+  prezzoFinale=prezzoFinale * 0.6 
+console.log("prezzo over 65" + prezzoFinale)
+}else{
+    console.log("prezzo non scontato" + prezzoFinale)
+
+}
+
+//calcolare prezzo biglietto ad ogni km //
+
