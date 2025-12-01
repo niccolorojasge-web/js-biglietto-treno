deef5092a0acc2c5ccf90ded4c42c779567f658f
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2025-12-01 16:49:57 +0100

    km percorsi

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //il programma dovra` chiedere i km che vuole percorrere e l`eta`//
-const etaPassegero =(prompt "quanti anni hai?")
+let etaPassegero = parseInt(prompt("quanti anni hai?"))
 console.log(etaPassegero);
 
+let kmPercorsi = parseInt(prompt("quanti km vuoi percorrere?"))
+console.log ( kmPercorsi);
+
+//se possiedi meno di 18 anni ci sara uno sconto del 20%//
+
+if (etaPassegero<18){
+    kmPercorsi
+} 
